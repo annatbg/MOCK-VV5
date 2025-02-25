05c62d65058adfaacd78753d00486148563f9d43
FabulousPlastic <tor.hogberg@gmail.com>
2025-02-25 16:14:09 +0100

    Small filestructure update, renamed some things for consistency, added logout to Sidebar

--- frontend/src/pages/Client.jsx
+++ frontend/src/pages/Client.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MainHeader from "../components/header/mainHeader/MainHeader";
-import PageNav from "../components/nav/PageNav/PageNav";
-import MainFooter from "../components/footer/mainFooter/MainFooter";
-import Main from "../components/main/Main";
 import useUser from "../store/useUser";
 import HomeView from "../views/client/homeView";
 import DemandsView from "../views/client/demandsView";
 import MatchView from "../views/client/matchView";
 import ProfileView from "../views/client/profileView";
+import NotificationsView from "../views/client/notificationsView";
 import Sidebar from "../components/sidebar/Sidebar";
 import "./styles/ClientPage.css";
 
@@ -16,7 +13,6 @@ import "./styles/ClientPage.css";
 
 function Client() {
   const user = useUser((state) => state.user);
-  const logout = useUser((state) => state.logout);  
   const navigate = useNavigate();
   const [activeView, setActiveView] = useState("home");
 
@@ -26,12 +22,6 @@ function Client() {
     }
   }, [user, navigate]);
 
-  const handleLogout = () => {
-    logout();
-    localStorage.removeItem("token");
-    navigate("/");
-    console.log("Logged out");
-  };
 
   const renderView = () => {
     switch (activeView) {
@@ -43,6 +33,8 @@ function Client() {
         return <MatchView />;
       case "demands":
         return <DemandsView />;
+      case "notifications":
+        return <NotificationsView />;  
       default:
         return <HomeView />;
     }
